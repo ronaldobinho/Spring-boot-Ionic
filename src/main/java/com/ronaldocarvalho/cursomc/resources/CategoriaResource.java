15a89abb5abc83e622ec22d69dc36f3557febaf8
ronaldobinho@gmail.com <ronaldobinho@gmail.com>
2018-03-29 23:49:52 -0300

    Classe de dominio Categoria

--- src/main/java/com/ronaldocarvalho/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/ronaldocarvalho/cursomc/resources/CategoriaResource.java
@@ -1,17 +1,30 @@
 package com.ronaldocarvalho.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ronaldocarvalho.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
 		
-		return "Rest funciona";
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		
+		
+		return lista;
 	}
 
 }
