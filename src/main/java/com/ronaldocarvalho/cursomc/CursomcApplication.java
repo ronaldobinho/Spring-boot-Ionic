e394401cf694abe03bf707bd6a6699b09cc4d85b
ronaldobinho@gmail.com <ronaldobinho@gmail.com>
2018-03-30 01:10:21 -0300

    Criando instanciacao para testes no h2

--- src/main/java/com/ronaldocarvalho/cursomc/CursomcApplication.java
+++ src/main/java/com/ronaldocarvalho/cursomc/CursomcApplication.java
@@ -1,12 +1,36 @@
 package com.ronaldocarvalho.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.ronaldocarvalho.cursomc.domain.Categoria;
+import com.ronaldocarvalho.cursomc.repositoiries.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.save(Arrays.asList(cat1, cat2));
+
+	}
+	
+	
 }
