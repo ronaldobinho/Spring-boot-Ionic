61cd71bcc2ae0b754a8ffd3ad39c026a8fab40ef
ronaldobinho@gmail.com <ronaldobinho@gmail.com>
2018-04-13 00:01:12 -0300

    Paginacao com parametros opcionais - Spring DATE

--- src/main/java/com/ronaldocarvalho/cursomc/CursomcApplication.java
+++ src/main/java/com/ronaldocarvalho/cursomc/CursomcApplication.java
@@ -71,6 +71,13 @@ public class CursomcApplication implements CommandLineRunner {
 
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
+		Categoria cat3 = new Categoria(null, "Cama Mesa E Banho");
+		Categoria cat4 = new Categoria(null, "Jardinagem");
+		Categoria cat5 = new Categoria(null, "Eletronicos");
+		Categoria cat6 = new Categoria(null, "Decoracao");
+		Categoria cat7 = new Categoria(null, "Perfumaria");
+		Categoria cat8 = new Categoria(null, "Ferramentas");
+		
 
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
@@ -83,7 +90,7 @@ public class CursomcApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat2, cat1));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 
-		categoriaRepository.save(Arrays.asList(cat1, cat2));
+		categoriaRepository.save(Arrays.asList(cat1, cat2, cat3, cat4,cat5,cat6,cat7,cat8));
 		produtoRepository.save(Arrays.asList(p1, p2, p3));
 
 		Estado est1 = new Estado(null, "Minas Gerais");
--- src/main/java/com/ronaldocarvalho/cursomc/services/CategoriaService.java
+++ src/main/java/com/ronaldocarvalho/cursomc/services/CategoriaService.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.ronaldocarvalho.cursomc.domain.Categoria;
@@ -55,5 +58,12 @@ public class CategoriaService {
 		return repo.findAll();
 		
 	}
+	
+	//Paginacao
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);	
+		
+		return repo.findAll(pageRequest);
+	} 
 	
 }
