e60a49f30033b6e414b5dbbde9166f2f0931a327
ronaldobinho@gmail.com <ronaldobinho@gmail.com>
2018-04-12 23:37:16 -0300

    Listando Categorias com DTO

--- src/main/java/com/ronaldocarvalho/cursomc/services/CategoriaService.java
+++ src/main/java/com/ronaldocarvalho/cursomc/services/CategoriaService.java
@@ -1,5 +1,7 @@
 package com.ronaldocarvalho.cursomc.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
@@ -48,4 +50,10 @@ public class CategoriaService {
 			throw new DataIntegrityExpetion("Nao é possivel deletar uma categoria que possui produtos");
 		}
 	}
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
+		
+	}
+	
 }
