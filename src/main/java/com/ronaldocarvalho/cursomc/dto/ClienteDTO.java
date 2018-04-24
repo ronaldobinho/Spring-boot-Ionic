8c2419fa3e71e4b4018859d5fd813686ea3faf66
ronaldobinho@gmail.com <ronaldobinho@gmail.com>
2018-04-24 00:22:13 -0300

    Validacao de email na atualizacao

--- src/main/java/com/ronaldocarvalho/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/ronaldocarvalho/cursomc/dto/ClienteDTO.java
@@ -7,7 +7,9 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import com.ronaldocarvalho.cursomc.domain.Cliente;
+import com.ronaldocarvalho.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
