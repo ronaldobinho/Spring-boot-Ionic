0b3009090dd8758e047b4b62c5d186617cdd3cbf
ronaldobinho@gmail.com <ronaldobinho@gmail.com>
2018-05-06 00:02:47 -0300

    Esqueci a senha

--- src/main/java/com/ronaldocarvalho/cursomc/services/EmailService.java
+++ src/main/java/com/ronaldocarvalho/cursomc/services/EmailService.java
@@ -4,17 +4,20 @@ import javax.mail.internet.MimeMessage;
 
 import org.springframework.mail.SimpleMailMessage;
 
+import com.ronaldocarvalho.cursomc.domain.Cliente;
 import com.ronaldocarvalho.cursomc.domain.Pedido;
 
 public interface EmailService {
-	
+
 	void sendOrderConfirmationEmail(Pedido obj);
-	
+
 	void sendEmail(SimpleMailMessage msg);
-	
+
 	void sendOrderConfirmationHtmlEmail(Pedido obj);
-	
+
 	void sendHtmlEmail(MimeMessage msg);
 
+	void sendNewPasswordEmail(Cliente cliente, String newPass);
 
 }
+	
